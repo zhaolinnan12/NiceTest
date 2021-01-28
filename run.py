872bc29a5a57533d0d790fec65c4f6751316cc0e
zhaolinnan <823012700@qq.com>
2021-01-28 15:21:57 +0800

    新增requirements

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from test.conftest import pytest
+from tools import logger
+from tools.read_file import ReadFile
+
+report = ReadFile.read_config('$.file_path.report')
+logfile = ReadFile.read_config('$.file_path.log')
+
+
+def run():
+    if os.path.exists('report/'):
+        shutil.rmtree(path='report/')
+    logger.add(logfile, enqueue=True, encoding='utf-8')
+    logger.info("""
+                 _    _         _      _____         _   
+  __ _ _ __ (_)  / \  _   _| |_ __|_   _|__  ___| |_ 
+ / _` | '_ \| | / _ \| | | | __/ _ \| |/ _ \/ __| __|
+| (_| | |_) | |/ ___ \ |_| | || (_) | |  __/\__ \ |_ 
+ \__,_| .__/|_/_/   \_\__,_|\__\___/|_|\___||___/\__|
+      |_|                                            
+      Starting      ...     ...     ...
+    """)
+    pytest.main(args=['test/test_api.py', f'--alluredir={report}/data'])
+    # 自动以服务形式打开报告
+    # os.system(f'allure serve {report}/data')
+
+    # 本地生成报告
+    os.system(f'allure generate {report}/data -o {report}/html --clean')
+    logger.success('报告已生成')
+
+
+if __name__ == '__main__':
+    run()
+
+
--- tools/read_file.py
+++ tools/read_file.py
@@ -7,7 +7,7 @@ class ReadFile:
     config_dict = None
 
     @classmethod
-    def get_config_dict(cls, config_path: str = '../config/config.yaml') -> dict:
+    def get_config_dict(cls, config_path: str = '/Users/colin/PycharmProjects/NiceTest/config/config.yaml') -> dict:
         """读取配置文件，并且转换成字典
         :config_path: 配置文件地址， 默认使用当前项目目录下的config/config.yaml
         """
@@ -18,7 +18,7 @@ class ReadFile:
         return cls.config_dict
 
     @classmethod
-    def read_config(cls, expr: str = '.') -> dict:
+    def read_config(cls, expr: str = '') -> dict:
         """默认读取config目录下的config.yaml配置文件，根据传递的expr jsonpath表达式可任意返回任何配置项
         :expr: 提取表达式, 使用jsonpath语法,默认值提取整个读取的对象
         """
@@ -27,18 +27,17 @@ class ReadFile:
     @classmethod
     def read_testcase(cls):
         """
-        读取excel格式的测试用例
-        :return: data_list - pytest框架在test_api执行parametrize参数化可用的数据
-        """
+         读取excel格式的测试用例
+         :return: data_list - pytest参数化可用的数据
+         """
         data_list = []
         book = xlrd.open_workbook(cls.read_config('$.file_path.test_case'))
         # 读取第一个sheet页
         table = book.sheet_by_index(0)
         for norw in range(1, table.nrows):
-            # 每行第4列 是否运行，判断接口是否执行
-            if table.cell_value(norw, 3) != '否':
+            # 每行第4列 是否运行
+            if table.cell_value(norw, 3) != '否':  # 每行第4列等于否将不读取内容
                 value = table.row_values(norw)
                 value.pop(3)
                 data_list.append(list(value))
         return data_list
-
